0284c912e9e1611545999e00da79fdc0411a7286
sirpoint <maximo.calero@sirpoint.com>
2018-11-20 07:52:07 +0100

    Removing component we are not going to use

--- src/docs/examples/HelloWorld/ExampleHelloWorld.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import HelloWorld from 'srp-react-library-js/HelloWorld';
-
-/** Custom message */
-export default function ExampleHelloWorld() {
-  return <HelloWorld message="Pluralsight viewers!" />
-}
--- src/docs/examples/TestComponent/ExampleTestComponent.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import TestComponent from 'srp-react-library-js/TestComponent';
-
-/** Custom message */
-export default function ExampleTestComponent() {
-  return <TestComponent title="test!" content ="test" />
-}
