722aa22a72dd3df1463331156d3cc211dcb077d2
Calero Sanchez Maximo (ERNI) <camx@erni-espana.es>
2018-12-01 07:17:46 +0100

    Changing GlobalNavigation component to styled-components

--- src/components/GlobalNavigation/GlobalNavigation.js
+++ src/components/GlobalNavigation/GlobalNavigation.js
@@ -1,32 +1,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './globalNavigation.module.css';
+import styled from 'styled-components';
 
 /** Component to show a menu bar */
 const GlobalNavigation = ({title, currentUrl, globalNavigationItems}) => {
+    const NavigationMainContainer  = styled.div `
+        margin-top: 5px;
+        margin-bottom: 5px;
+    `
+
+    const NavigationIcon = styled.span `
+        margin-left: 5px;
+        font-size: 40px;
+        width: 100%;
+    `
+
+    const NavigationIconDiv = styled.div `
+        width: 3%;
+        float: left;
+        font-size: 40px;
+        font-weight: 900;
+        text-align: center;    
+    `
+
+    const NavigationContainer = styled.ul `
+        list-style-type: none;
+        margin-top: 6px;
+        width: 85%;
+        padding: 0;
+        overflow: hidden;
+        height: 40px;
+        font-size: 1.15em;
+        color: rgb(102, 102, 102);
+        float: left;
+        width:80%;    
+    `
+
+    const NavigationItem = styled.li `
+        float: left;
+
+        :hover {
+            color: rgb(0, 120, 212);
+        }            
+    `
+
+    const NavigationItemLink = styled.a`
+        display: block;
+        text-align: center;
+        padding: 5px 16px;
+        text-decoration: none;
+        color: rgb(102, 102, 102);
+    `
     return (
-        <div className={styles.divContainer}>
-        <div className={styles.marginDiv}>
-            <span className= {`ms-Icon ms-Icon--Family ${styles.menuIcon}`} title={title}></span>
-        </div>
-        <ul className={styles.navigationBar}>
-            {Object.keys(globalNavigationItems).map(key=> {
-                if(currentUrl === globalNavigationItems[key].url){
-                    return(
-                        <li className={styles.navigationItemSelected} key={key}>
-                            <span>{globalNavigationItems[key].name}</span>
-                        </li>
-                    );
-                }else{
-                    return(
-                        <li className={styles.navigationItem} key={key}>
-                            <a className={styles.navigationItem} href={globalNavigationItems[key].url}>{globalNavigationItems[key].name}</a>
-                        </li>   
-                    );
-                }
-            })}
-        </ul>
-    </div> 
+        <NavigationMainContainer>
+            <NavigationIconDiv>
+                <NavigationIcon className= {`ms-Icon ms-Icon--Family ${styles.menuIcon}`} title={title}></NavigationIcon>
+            </NavigationIconDiv>
+            <NavigationContainer>
+                {Object.keys(globalNavigationItems).map(key=> {
+                    if(currentUrl === globalNavigationItems[key].url){
+                        return(
+                            <NavigationItem key={key}>
+                                <span>{globalNavigationItems[key].name}</span>
+                            </NavigationItem>
+                        );
+                    }else{
+                        return(
+                            <NavigationItem key={key}>
+                                <NavigationItemLink href={globalNavigationItems[key].url}>{globalNavigationItems[key].name}</NavigationItemLink>
+                            </NavigationItem>   
+                        );
+                    }
+                })}
+            </NavigationContainer>
+        </NavigationMainContainer> 
     )
 }
 
